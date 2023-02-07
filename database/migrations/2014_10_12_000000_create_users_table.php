71b13f05d8d932769d0ac10eebaeade8420945f2
okulafebrian <okulafebrian@gmail.com>
2023-02-07 20:43:18 +0700

    new -vincent

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles')->onUpdate('cascade')->onDelete('cascade');
+            $table->unsignedBigInteger('gender_id');
+            $table->foreign('gender_id')->references('id')->on('genders')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('photo');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@ use App\Http\Controllers\HomeController;
 */
 
 Route::get('/',[HomeController::class , 'showWelcomePage']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
